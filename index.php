c1339ca24866e8bb1bb80d9eb3762f8c7b7ed901
workermoon <maanworkerone@gmail.com>
2025-02-10 14:03:22 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP App</title>
+</head>
+<body>
+    <?php
+        echo "This is powered by PHP.";
+    ?>    
+</body>
+</html>
